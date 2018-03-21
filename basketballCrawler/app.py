0094c90065c11b9359f770bf98bd191ad5215dcd
Zhang San <abc@abc.com>
2018-03-20 18:50:25 -0700

    can fix player name in ambigous

--- basketballCrawler/app.py
+++ basketballCrawler/app.py
@@ -27,17 +27,43 @@ app.secret_key = "super secret key"
 
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-#CmdList = {'show system internal vpcm info peer-link':['show system internal vpcm info peer-link'],
-#           'show system internal vpcm info vpc':['show system internal vpcm info vpc'],
-#           'show system internal vpcm info global':['show system internal vpcm info global']
-#           }
+import re
+from collections import Counter
 
+def words(text): return re.findall(r'\w+', text.lower())
 
+WORDS = Counter(words(open('little.txt').read()))
 
+def P(word, N=sum(WORDS.values())): 
+    "Probability of `word`."
+    return WORDS[word] / N
 
-# This route will show a form to perform an AJAX request
-# jQuery is loaded to execute the request and update the
-# value of the operation
+def correction(word): 
+    "Most probable spelling correction for word."
+    return max(candidates(word), key=P)
+
+def candidates(word): 
+    "Generate possible spelling corrections for word."
+    return (known([word]) or known(edits1(word)) or known(edits2(word)) or [word])
+
+def known(words): 
+    "The subset of `words` that appear in the dictionary of WORDS."
+    return set(w for w in words if w in WORDS)
+
+def edits1(word):
+    "All edits that are one edit away from `word`."
+    letters    = 'abcdefghijklmnopqrstuvwxyz'
+    splits     = [(word[:i], word[i:])    for i in range(len(word) + 1)]
+    deletes    = [L + R[1:]               for L, R in splits if R]
+    transposes = [L + R[1] + R[0] + R[2:] for L, R in splits if len(R)>1]
+    replaces   = [L + c + R[1:]           for L, R in splits if R for c in letters]
+    inserts    = [L + c + R               for L, R in splits for c in letters]
+    return set(deletes + transposes + replaces + inserts)
+
+def edits2(word): 
+    "All edits that are two edits away from `word`."
+    return (e2 for e1 in edits1(word) for e2 in edits1(e1))
+
 @app.route('/search/', methods=['GET', 'POST'])
 def search():
     print request.method
@@ -198,7 +224,13 @@ def result():
             result_temp = 'Not found!'
 
     if radio_value == 'all':
-        cursor = collection.find({'$text':{'$search': _name}}, {'_id':0})
+        _name = _name.lower()
+        _name1 = correction(_name)
+
+        if _name1 != _name:
+            result_temp += 'Do you mean '+_name1 + ':\n'
+
+        cursor = collection.find({'$text':{'$search': _name1}}, {'_id':0})
 
         #result_temp = ' has following players:\n'
 
